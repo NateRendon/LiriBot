181ed1b5499bb841b7515db6620267529d120b15
Nathan Rendon <naterendon@gmail.com>
2020-01-23 13:03:10 -0600

    changed up code to be more organized and easily read

--- liri.js
+++ liri.js
@@ -1,143 +1,174 @@
-//keys stored here
-requestAnimationFrame("dotenv").config();
+//Include axios npm package
+const axios = require('axios');
+const moment = require('moment');
+const fs = require('fs');
+const Spotify = require('node-spotify-api');
 
-//npm - packages and required files
-var Spotify = require('node-spotify-api');
-var request = require('request');
-var terminalLink = require('terminal-link');
+//Read and Set Environment variables
+require("dotenv").config();
 
-let fs = require("fs");
-let keys = require("./keys");
-let data = fs.readFileSync("./random.txt", "utf8");
+//Import keys
+const keys = require("./keys.js");
 
-//keys
-let spotify = new Spotify(keys.spotify);
+//Access Spotify keys
+var spotify = new Spotify(keys.spotify);
 
-//logs user input
-let search = process.argv[2];
-let entireCmdLine = process.argv;
-let findThis = "";
+const divider = "\n------------------------------------------------------------------------------------\n";
 
-//Text Colors
-const FgBlue = "\x1b[34m";
-const FgWhite = "\x1b[0m";
-const FgCyan = "\x1b[36m";
-const FgGreen = "\x1b[32m";
-const FgMagenta = "\x1b[35";
+//Get parameters and store it in a variable
+let command = process.argv[2];
+let search = process.argv[3];
 
-for (let i = 3; i <entireCmdLine.length; i++) {
-    if (i > 3 && i < entireCmdLine.length) {
-        findThis = findThis + "+" + entireCmdLine[i];
-    } else {
-        findThis += entireCmdLine[i];
-    };
-};
+console.log(`Action: ${command}`);
+console.log(`Search: ${search}`);
 
-//searches results!
-function liriBot() {
-    switch(search) {
-        case "spotify-this-song":
+processCommand();
 
-        var logIt = search.concat("," + findThis + ",")
-
-        if (findThis === "") {
-            findThis = "the+sign+ace+of+base" // if no song is typed in, the program will output data for the song
-        }
-
-        console.log("searching" + FgGreen + "Spotify..." + FgWhite + "\n");
-
-        spotify.search({type: 'track', query: findThis}, function(err, data) {
-            if(err) {
-                return console.log('Error occured: ' + err);
+function processCommand() {
+    switch(command) {
+        case 'concert-this':
+            let artist = search;
+            if(artist === undefined) {
+                artist = "Ace of Base"
             }
-
-            console.log(FgGreen + "-----------Spotify Search Results!-0------------\n" +FgWhite);
-            console.log(FgGreen + 'Artist(s): ' + FgWhite + data.tracks.items[0].artists[0].name);
-            console.log(FgGreen + 'Song: ' + FgWhite +data.tracks.items[0].name);
-            console.log(FgGreen + 'Album: ' + FgWhite + data.tracks[0].album.name);
-            console.log(terminalLink(FgGreen + 'Preview' + FgWhite, daya.tracks.items[0].artists[0].external_urls.spotify));
-            console.log(FgGreen + "\n-----------------------------" + Fgwhite);
-            fs.appendFile("random.txt", logIt, function(err) {
-                if (err) {
-                    return console.log(err);
-                }
-                console.log("\nrandom.txt was updated!\n");
-            });
-        });
-        break;
-
-        case "movie-this":
-
-        if (findThis === "") {
-            findThis = "Monty+Python+and+the+Holy+Grail" // if the user doesnt type in a movie in, it will search for monty python and the holy grail
-        }
-
-        var queryUrl = "http://www.omdbapi.com/?t=" + findThis + "&y=&plot=short&apikey=" + keys.omdb.key;
-        var logIt = search.concat("," + findThis + ",")
-
-        console.log("Searching " + FgBlue + "OMBD.." + FgWhite + "\n")
-
-        request(queryUrl, function(error, response, body) {
-
-            if (error) {
-                console.log(error);
-            } else if (!error && response.statusCode === 200 && JSON.parse(body).Ratings !== undefined) {
-                console.log(FgBlue + "----------Your Movie Search Results----------\n" + FgWhite);
-                    console.log(FgBlue + "Year: " + FgWhite + JSON.parse(body).Year);
-                    console.log(FgBlue + "Title: " + FgWhite + JSON.parse(body).Title);
-                    console.log(FgBlue + "Actors: " + FgWhite + JSON.parse(body).Actors);
-                    console.log(FgBlue + "Country: " + FgWhite + JSON.parse(body).Country);
-                    console.log(FgBlue + "Language: " + FgWhite + JSON.parse(body).Language);
-                    console.log(FgBlue + "IMDB Rating: " + FgWhite + JSON.parse(body).imdbRating);
-                    console.log(FgBlue + "Rotten Tomatoes Rating: " + FgWhite + JSON.parse(body).Ratings[1].Value);
-                    console.log(FgBlue + "Plot: " + FgWhite + JSON.parse(body).Plot);
-                    console.log(FgBlue + "\n---------------------------------------------" + FgWhite);
-                    fs.appendFile("random.txt", logIt, function(err) {
-                        if (err) {
-                            return console.log(err);
-                        }
-                        console.log("\nrandom.txt was updated!\n");
-                    });
-                } else {
-                    console.log("That movie does not exists in the " + FgBlue + " OMBD.." + FgWhite + "\n")
-                }
-        });
-        break;
-
-        case "do-what-it-says":
-
-        fs.readFile("random.txt", "utf8", function(error, data) {
-
-            var dataArr = data.split(",");
-            var randomPull = Math.floor(Math.random() * (dataArr.length - 1));
-            console.log("You randomly pulled with number: " + rnadomPull + "\n");
-
-            if(error) {
-                return console.log(error);
-            }
-
-            if (randomPull % 2 !== 0) {
-                earch = dataArr[randomPull];
-                findThis = dataArr[randomPull + 1];
-            } else if (randomPull % 2 === 0 && randomPull !== 0) {
-                search = dataArr[randomPull - 1];
-                findThis = dataArr[randomPull];
-            } else {
-                search = dataArr[randomPull];
-            }
-
-            console.log("Your " + FgGreen + "R" + FgCyan + "a" + FgBlue + "n" + FgMagenta + "d" + FgBlue + "o" + FgCyan + "m" + FgWhite + " search has begun.\n")        
-        
-            liriBot();
-        });
-        
-        break;
-
-        default:
-            cconsole.log("Not a valid input. Please use the following options: " + FgGreen + "\n * spotify-this-song " + FgCyan + "\n * my-tweets " + FgBlue + "\n * movie-this " + FgMagenta + "\n * do-what-it-says" + FgWhite)
+            searchConcert(artist);
             break;
-        };
-};
 
+        case 'spotify-this-song':
+            let song = search;
+            if(song === undefined) {
+                song = "The Sign"
+            }
+            searchSong(song);
+            break;
 
-liriBot();
+        case 'movie-this':
+            let movie = search;
+            if(movie === undefined) {
+                movie = "Mr. Nobody"
+            }
+            searchMovie(movie);
+            break;
+
+        case 'do-what-it-says':
+            executeFile();
+            break;
+    }
+}
+
+function searchConcert(artist) {
+    let combineArtist = artist.replace(/['"]+/g, '').split(' ').join('+');
+
+    var queryUrl = "https://rest.bandsintown.com/artists/" + combineArtist + "/events?app_id=codingbootcamp";
+
+    axios
+        .get(queryUrl)
+        .then(function(response) {
+            let showData = "Concerts for: " + artist + "\n\n";
+
+            for (const concert of response.data) {
+                showData += [
+                    "Name of the Venue: " + concert.venue.name,
+                    "Venue Location: " + concert.venue.city + ", " + concert.venue.country,
+                    "Date of the Event: " + moment(concert.datetime).format("MM/DD/YYYY"),
+                    divider
+                ].join("\n\n");
+            }
+            
+            console.log(showData);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+}
+
+function searchSong(song) {
+    let combinedSong = song.replace(/['"]+/g, '').split(' ').join('+');
+
+    spotify
+        .search({ type: 'track', query: combinedSong })
+        .then(function(response) {
+            let showData = "";
+            for (const song of response.tracks.items) {
+                let artists = "";
+
+                //Artists is an array of objects
+                if (song.artists && song.artists.length > 0) {
+                    song.artists.find(function(artist) {
+                        artists += artist.name + ", "
+                    });
+                }
+
+                showData += [
+                    "Artist(s): " + artists.replace(/,\s*$/, ''),
+                    "Name of Song: " + song.name,
+                    "Spotify Song Preview: " + (song.preview_url === null ? "" : song.preview_url),
+                    "Album Name: " +  (song.album === null ? "" : song.album.name),
+                    divider
+                ].join("\n\n");
+            }
+            
+            console.log(showData);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+}
+
+function searchMovie(movie) {
+    let combinedMovie = movie.replace(/['"]+/g, '').split(' ').join('+');
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + combinedMovie + "&y=&plot=short&apikey=trilogy";
+
+    axios
+        .get(queryUrl)
+        .then(function(response) {
+            let imdbRating = "";
+            let rottenRating = "";
+
+            //Ratings is an array of objects
+            if (response.data.Ratings && response.data.Ratings.length > 0) {
+                response.data.Ratings.find(function(rating) {
+                    if (rating.Source === "Internet Movie Database") {
+                        imdbRating = rating.Value;
+                    }
+                    else if (rating.Source === "Rotten Tomatoes") {
+                        rottenRating = rating.Value;
+                    }
+                });
+            }
+
+            let showData = [
+                "Movie Title: " + response.data.Title,
+                "Year Released: " + response.data.Year,
+                "IMDB Rating: " + imdbRating,
+                "Rotten Tomatoes Rating: " + rottenRating,
+                "Country Where Produced: " + response.data.Country,
+                "Language: " + response.data.Language,
+                "Plot: " + response.data.Plot,
+                "Actors: " + response.data.Actors,
+                divider
+            ].join("\n\n");
+            
+            console.log(showData);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+}
+
+function executeFile() {
+
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) return console.log(err);
+
+        const lines = data.trim().split('\n');
+        
+        for(const line of lines) {
+            let result = line.split(',')
+            command = result[0];
+            search = result[1];
+            processCommand();
+        }
+    });
+}
